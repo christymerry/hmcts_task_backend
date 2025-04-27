08cbc9509e26fc93776910b407367dcac63a47bb
christymerry <christymerry97@gmail.com>
2025-04-27 19:31:25 +0100

    Bug Fixes@

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const taskRoutes = require('./routes/task.routes');
 require('./db');
 
 app.use(express.json());
+app.use(cors())
 app.use('/api/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 3000;
--- models/task.model.js
+++ models/task.model.js
@@ -4,7 +4,7 @@ const TaskModel = {
     create:(task,response) =>{
         db.query(
             'INSERT INTO tasks (title, description, status, due_date) VALUES (?,?,?,?)',
-            [task.title, task.description, task.status, task.due_date],
+            [task.title, task.description, 'New', task.due_date],
             response
         )
     },
